34f953c1266f3d9de103ac8964a435f8c5fb5989
nkonovalova <konata.work@gmail.com>
2025-02-07 12:35:56 +0100

    Add json-server. Mock data

---
+++ mock/db.cjs
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+const dir = __dirname;
+const data = {
+    monsters: JSON.parse(fs.readFileSync(dir + '/data/monsters.json', 'utf8')),
+    monstersList: JSON.parse(fs.readFileSync(dir + '/data/monstersList.json', 'utf8')),
+};
+
+
+module.exports = () => data;
